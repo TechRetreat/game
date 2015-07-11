a139be773990f83b7ac5a83fa0afd5f3e6d57a0d
Yu Chen Hou <houyuchen66@gmail.com>
2015-07-11 00:39:50 -0400

    refactored into class

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,15 +5,10 @@ class SiteController < ApplicationController
   def index
     brain_paths = []
     options = { :width => 800, :height => 600, :max_ticks => 1200 , :gui => false, :gc => true , :replay_dir => 'replays' , :seed => Kernel.srand}
-    runner = RTanque::Recorder.create_runner(options)
-    brain_paths.each do |brain_path|
-      begin
-        runner.add_brain_path(brain_path)
-      rescue RTanque::Runner::LoadError => e
-        say e.message, :red
-        exit false
-      end
-    end
-    runner.start(false)
+
+    game_ctrl = GameController.new(options)
+    game_ctrl.add_ai_list(brain_paths)
+    game_ctrl.simulate
+
   end
 end
