cc629407f95571b5e72c588b20a3f8cee2b2426f
Yu Chen Hou <houyuchen66@gmail.com>
2015-07-15 02:16:02 -0400

    turn tank into a CRUD resource

--- app/controllers/tanks_controller.rb
+++ app/controllers/tanks_controller.rb
@@ -1,4 +1,74 @@
 class TanksController < ApplicationController
+  before_action :set_tank, only: [:show, :edit, :update, :destroy]
+
+  # GET /tanks
+  # GET /tanks.json
   def index
+    @tanks = Tank.all
   end
+
+  # GET /tanks/1
+  # GET /tanks/1.json
+  def show
+  end
+
+  # GET /tanks/new
+  def new
+    @tank = Tank.new
+  end
+
+  # GET /tanks/1/edit
+  def edit
+  end
+
+  # POST /tanks
+  # POST /tanks.json
+  def create
+    @tank = Tank.new(tank_params)
+
+    respond_to do |format|
+      if @tank.save
+        format.html { redirect_to @tank, notice: 'Tank was successfully created.' }
+        format.json { render :show, status: :created, location: @tank }
+      else
+        format.html { render :new }
+        format.json { render json: @tank.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /tanks/1
+  # PATCH/PUT /tanks/1.json
+  def update
+    respond_to do |format|
+      if @tank.update(tank_params)
+        format.html { redirect_to @tank, notice: 'Tank was successfully updated.' }
+        format.json { render :show, status: :ok, location: @tank }
+      else
+        format.html { render :edit }
+        format.json { render json: @tank.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /tanks/1
+  # DELETE /tanks/1.json
+  def destroy
+    @tank.destroy
+    respond_to do |format|
+      format.html { redirect_to tanks_url, notice: 'Tank was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tank
+      @tank = Tank.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def tank_params
+      params.require(:tank).permit(:name)
+    end
 end
