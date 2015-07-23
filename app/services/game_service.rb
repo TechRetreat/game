1b81fd63317a7aa68c0f07e4b9d1c0913ce90822
jgzuke <jgzuke@gmail.com>
2015-07-23 19:59:38 -0400

    added counter to send 50 tick json packets of data

--- app/services/game_service.rb
+++ app/services/game_service.rb
@@ -1,9 +1,15 @@
 require 'rtanque/runner'
+require 'json'
 
 class GameService
+  attr_reader :ticks_since_send, :tick_batch_data_array
+
   @queue = :match_runner
 
   def self.perform(match_id)
+    @ticks_since_send = 0
+    @tick_batch_data_array = Array.new
+
     # match = Match.find match_id
 
     # TODO: add tanks and run game
@@ -23,6 +29,16 @@ class GameService
     runner.match.after_tick = proc do |match|
       # TODO: batch ticks and send useful data
       channel.trigger :tick, tick: match.ticks
+
+      @tick_batch_data_array.push ticks: match.ticks
+
+      @ticks_since_send += 1
+      if @ticks_since_send > 50
+        tick_batch_data_json = @tick_batch_data_array.to_json
+        channel.trigger :tick_batch, tick_batch: tick_batch_data_json
+        @ticks_since_send = 0
+        @tick_batch_data_array = Array.new
+      end
     end
 
     runner.match.after_stop = proc do |match|
