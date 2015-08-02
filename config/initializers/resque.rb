5d9fdd645eafe376f08d78068d687ae0d336812a
Yu Chen Hou <houyuchen66@gmail.com>
2015-08-02 19:06:27 -0400

    redis set up again

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -2,5 +2,4 @@ rails_root = ENV['RAILS_ROOT'] || File.dirname(__FILE__) + '/../..'
 rails_env = ENV['RAILS_ENV'] || 'development'
 
 redis_config = YAML.load(ERB.new(File.read("#{rails_root}/config/redis.yml")).result)[rails_env]
-#Resque.redis = "#{redis_config[:host]}:#{redis_config[:port]}"
-Resque.redis = ENV['REDIS_URL']
+Resque.redis = "#{redis_config[:host]}:#{redis_config[:port]}"
