e1e3ff9907727ac94883896947869feb81f2e3a5
Yu Chen Hou <houyuchen66@gmail.com>
2015-08-02 20:49:10 -0400

    redis set up again

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec rails server -p $PORT -e $RACK_ENV
-worker: QUEUE=* rake environment resque:work
+worker: QUEUE=* rake environment resque:work -c='./.resque'
--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,4 +3,4 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 
 redis_config = YAML.load(ERB.new(File.read("#{rails_root}/config/redis.yml")).result)[rails_env]
 #Resque.redis = "#{redis_config[:host]}:#{redis_config[:port]}"
-Resque.redis = "172.17.1.38:6379}"
+Resque.redis = "172.17.1.38:6379"
