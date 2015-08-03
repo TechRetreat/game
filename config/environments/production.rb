cd48d788135e46b8fc7b5fd97f81bd33aa854f43
Yu Chen Hou <houyuchen66@gmail.com>
2015-08-03 02:36:44 -0400

    more mailing host setup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mail settings
-  config.action_mailer.default_url_options = { :host => 'staging.techtanks.techretreat.ca' }
+  config.action_mailer.default_url_options = { :host => 'staging. techtanks.techretreat.ca' }
 
   # Default Mailer Host
   Rails.application.routes.default_url_options[:host] = 'staging.techtanks.techretreat.ca'
