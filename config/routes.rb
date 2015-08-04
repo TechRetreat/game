f92a7303b36c82f9fd276b938bf10a3ec741bbe3
Yu Chen Hou <houyuchen66@gmail.com>
2015-08-03 22:39:52 -0400

    fix for admin check

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   # resque-web config
   resque_web_constraint = lambda do |request|
     current_user = request.env['warden'].user
-    current_user.present? && current_user.respond_to?(:is_admin?) && current_user.is_admin?
+    current_user.present? && current_user.admin?
   end
 
   constraints resque_web_constraint do
